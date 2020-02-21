89dcc76d417b0400563ddf3a969e884863c8383d
Vaishak Anand <vaishak140498@gmail.com>
2020-02-21 13:52:51 +0800

    minor changes

--- Loading.js
+++ Loading.js
@@ -1,11 +1,21 @@
 var submitBut = document.getElementById("submitButton");
 var submit = document.getElementById("submiter");
 var submitText = document.getElementById("submitText");
+
 submitBut.addEventListener("click", function() {
    submit.classList.add("spinner-border");
    submit.classList.add("spinner-border-sm");
    submitText.textContent = " Processing...";
    setTimeout(function(){
     window.location.href = 'PostProcessing.html';
- }, 1500);
+ }, 1800);
+});
+
+var uploadBut = document.getElementById("uploadBut");
+var uploadText = document.getElementById("upload");
+
+uploadBut.addEventListener("click", function(){
+   setTimeout(function(){
+      uploadText.textContent = "SampleContract.pdf uploaded";
+   }, 2500);
 });
